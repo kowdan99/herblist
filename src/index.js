52aef09b7b58244d66b26d92da91af658a274e41
Youssof Kowdan <kowday@uw.edu>
2021-03-22 23:07:55 -0700

    fixed some stuff

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import Anx from './anxiety'
 import IMMUNITY from './immunity'
-
+import registerServiceWorker from './registerServiceWorker';
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
@@ -25,6 +25,7 @@ ReactDOM.render(
   rootElement
 );
 
+registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
